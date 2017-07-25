dd32335dd3b4950f861e7b1d831fd2bda67f6b8a
Mahmoud <mahmoudmosallamy@hotmail.com>
2017-07-25 19:10:39 +1000

    Add new class Feelings

---
+++ core/src/com/muyagdx/blast/Feelings.java
@@ -0,0 +1,9 @@
+package com.muyagdx.blast;
+
+public class Feelings {
+
+	public Feelings() {
+		// Characters and enemies have feelings, some reached randomly and some triggered, abilities are based on current feeling. Possible mixed feelings (limit break)
+	}
+
+}
